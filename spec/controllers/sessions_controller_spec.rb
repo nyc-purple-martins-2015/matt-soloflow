afe220a5105d9b3d08865d5115c000036c859cbc
Matthew LeFurge <email": null>
2015-11-13 18:29:36 -0500

    finish tests for session controller

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -14,6 +14,11 @@ describe SessionsController do
       expect(response).to be_success
     end
 
+    it 'creates a new user' do
+      get :new
+      expect(@user).to be_a_kind_of(User)
+    end
+
   end
 
   context "#create" do
@@ -24,10 +29,22 @@ describe SessionsController do
       expect(session[:user_id]).to eq @user.id
     end
 
-    it 'it does not log in an invalid user' do
+     it 'redirects to question path when user logs in' do
+      user_hash = {username: @user.username, email: @user.email, password: @user.password}
+      post :create, user_hash
+      expect(response).to redirect_to(questions_path)
+    end
+
+    it 'does not log in an invalid user' do
       user_hash = {username: @user.username, email: @user.email, password: "incorrect"}
       post :create, user_hash
       expect(session[:user_id]).to eq nil
+    end
+
+    it 'redirects to login path when authentication fails' do
+      user_hash = {username: @user.username, email: @user.email, password: "incorrect"}
+      post :create, user_hash
+      expect(response).to redirect_to(login_path)
     end
 
   end
@@ -39,6 +56,13 @@ describe SessionsController do
       post :create, user_hash
       get :destroy
       expect(session[:user_id]).to eq nil
+    end
+
+    it 'redirects to the questions path' do
+      user_hash = {username: @user.username, email: @user.email, password: @user.password}
+      post :create, user_hash
+      get :destroy
+      expect(response).to redirect_to(questions_path)
     end
 
   end
