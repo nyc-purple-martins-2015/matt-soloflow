e85dc509be180576af7f7e46938309ef531dec60
maecapozzi <maecapozzi@gmail.com>
2015-11-13 12:09:24 -0500

    fix sessions_controller rspec, add create test in users_controller spec

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -9,14 +9,5 @@ describe SessionsController do
     end
   end
 
-  context "#create" do
-    let(:user) { FactoryGirl.create :user }
-    subject { post :create, :user => { username: user.username, password: user.password, email: user.email } }
-
-    it "redirects to root path if correct credentials" do
-      expect(subject).to redirect_to questions_path(assigns[:user])
-    end
-  end
-
 end
 
