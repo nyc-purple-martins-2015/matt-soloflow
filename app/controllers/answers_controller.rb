a5264480ab170e920762ce0dfd04db2c256d7830
Alexis La Porte <aklaporte@gmail.com>
2015-11-13 16:14:29 -0500

    add create route, and view for answers

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,9 +1,33 @@
 class AnswersController < ApplicationController
 
-def new
-  @answer = Answer.new
-end
+  def new
+    @answer = Answer.new
+    @question = Question.find(params[:question_id])
+  end
 
+  def create
+    @user = User.find(session[:user_id])
+    @question = Question.find(params[:question_id])
+    #TODO: Refactor line 12 to not be hardcoded.
+    @answer = @question.answers.new(content: answer_params[:content], user_id: @user.id)
+    if @answer.save!
+      redirect_to question_path(@question)
+    else
+      render :new
+    end
+  end
 
+  def show
 
+  end
+
+  private
+
+  def answer_params
+    params.require(:answer).permit(:user_id, :content)
+  end
+
+ # def question_params
+    # params.require(:question).permit(:title, :content)
+  # end
 end
