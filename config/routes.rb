ee4e267d22f2bf1a1c0869b2e591147c5762cf75
Alexis La Porte <aklaporte@gmail.com>
2015-11-13 11:40:04 -0500

    add link_to in header to take user back to roo

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,8 @@
 Rails.application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-  # root :to => redirect 'questions#index'
-
   resources :questions do
   end
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'questions#index'
 
   resources :users
   get 'register' => 'users#new'
