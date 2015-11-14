d58436823fabfe597599e778edfde24a406d6a85
Matthew LeFurge <email": null>
2015-11-13 20:39:31 -0500

    add question comment logic and view

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,25 @@
 class CommentsController < ApplicationController
 
-def create
+  def create
+    @user = current_user
+    @comment = current_user.comments.new(comment_params)
+    if @comment.save
+      if @comment.commentable_type == "Answer"
+        redirect_to question_path(@comment.commentable.question_id)
+       else
+        redirect_to question_path(@comment.commentable_id)
+      end
+    else
+      redirect_to root_path
+    end
+  end
 
-end
+private
+
+  def comment_params
+    params.require(:comment).permit(:content, :commentable_type, :commentable_id)
+  end
+
 
 end
 
