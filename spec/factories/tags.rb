466db24a928b5774eb9f5a1556ad1e2eb1942711
Mae Capozzi <email": null>
2015-11-14 17:21:00 -0500

    create factory

--- spec/factories/tags.rb
+++ spec/factories/tags.rb
@@ -1,6 +1,8 @@
 FactoryGirl.define do
   factory :tag do
-    
+    factory :question do |f|
+    f.title { Faker::Lorem.sentence}
+    f.user_id { 1 }
+    f.content { Faker::Lorem.paragraph }
   end
-
 end
