09ec48e51e099d785f5038874fb14b928f0dac3b
Mae Capozzi <email": null>
2015-11-14 17:13:23 -0500

    fix spacing

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -4,7 +4,6 @@ RSpec.describe Question, type: :model do
   it "has a valid factory" do
     FactoryGirl.create(:question).should be_valid
   end
-
   it "is invalid without a title" do
     FactoryGirl.build(:question, title: nil).should_not be_valid
   end
